67e4de5eb47d1daea79243e53a61bf50b184076c
sky <972653707@qq.com>
2019-07-28 18:15:47 +0800

    join base jwt

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->group(['prefix' => 'auth', 'namespace' => 'Auth'], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('me', 'AuthController@me');
+});
